a982c8c3a1908c9291eefbecf67a93213b7fb348
Zafeer Mahmood <zafeer7460@gmail.com>
2023-09-30 19:52:47 +0500

    added jwt verification from auth2 for reqeusts

--- server/server.py
+++ server/server.py
@@ -1,23 +1,92 @@
 import os
 from flask import Flask, request, jsonify, send_from_directory,send_file,make_response
 from flask_cors import CORS
+import jwt  # requires cryptography
+from functools import wraps
+from dotenv import load_dotenv
+import tracemalloc
+
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
-
 CORS(app)
 
+load_dotenv(dotenv_path='./.env.local')
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
+tracemalloc.start()
+port = int(os.getenv('API_PORT', 3001))
+baseUrl = os.getenv('AUTH0_BASE_URL')
+issuerBaseUrl = os.getenv('AUTH0_ISSUER_BASE_URL')
+audience = os.getenv('AUTH0_AUDIENCE')
+client_id=os.getenv('AUTH0_CLIENT_ID')
+
+
+if not baseUrl or not issuerBaseUrl:
+    raise Exception('Please make sure that the file .env.local is in place and populated')
+
+if not audience:
+    print('AUTH0_AUDIENCE not set in .env.local. Shutting down API server.')
+    exit(1)
+
+def validate_access_token(access_token):
+    okta_auth_server =f'{issuerBaseUrl}/'
+    jwks_url = f"{issuerBaseUrl}/.well-known/jwks.json"
     try:
-        user_id = request.form.get('userId')
+        jwks_client = jwt.PyJWKClient(jwks_url, cache_jwk_set=True, lifespan=360)
+        signing_key = jwks_client.get_signing_key_from_jwt(access_token)
+        data = jwt.decode(
+            access_token,
+            signing_key.key,
+            algorithms=["RS256"],
+            issuer=okta_auth_server,
+            audience=f"{audience}",
+            options={
+                "verify_signature": True,
+                "verify_exp": True,
+                "verify_nbf": True,
+                "verify_iat": True,
+                "verify_aud": True,
+                "verify_iss": True,
+            },
+        )
+        return data
+    except jwt.exceptions.PyJWTError as err:
+        return False
+    
+def check_jwt(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        authorization_header = request.headers.get('Authorization')
+        if not authorization_header:
+            return jsonify({'message': 'Authorization token is missing'}), 401
+        parts = authorization_header.split()
+        if len(parts) != 2 or parts[0].lower() != 'bearer':
+            return jsonify({'message': 'Invalid token format'}), 401
+
+        token = parts[1]
+
+        validated_data = validate_access_token(token)
+        if not validated_data:
+            return jsonify({'message': 'Invalid token'}), 401
+
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+@app.route('/upload/<user_id>', methods=['POST'])
+@check_jwt
+def upload_file(user_id):
+    print(user_id)
+    try:
         if not user_id:
             return jsonify({'error': 'User ID is missing in the request'}), 400
 
         user_folder = os.path.join(app.config['UPLOAD_FOLDER'], user_id)
         if not os.path.exists(user_folder):
             os.makedirs(user_folder)
-        files = request.files.getlist('files')
+
+        files = request.files.getlist('files') 
+        print(files)
         for file in files:
             filename = os.path.join(user_folder, file.filename)
             file.save(filename)
@@ -57,6 +126,7 @@ def generate_folder_structure(root_path):
 
 
 @app.route('/get_folder_structure/<user_id>', methods=['GET'])
+@check_jwt
 def get_folder_structure(user_id):
     user_folder = os.path.join(app.config['UPLOAD_FOLDER'], user_id)
 
@@ -69,6 +139,7 @@ def get_folder_structure(user_id):
 
 
 @app.route('/download/<user_id>/<file_name>', methods=['GET'])
+@check_jwt
 def download_file(user_id, file_name):
     user_folder = os.path.join(app.config['UPLOAD_FOLDER'], user_id)
     file_path = os.path.join(user_folder, file_name)
@@ -84,16 +155,14 @@ def download_file(user_id, file_name):
 
 
 @app.route('/open/<user_id>/<file_name>', methods=['GET'])
+@check_jwt
 def open_file(user_id, file_name):
     user_folder = os.path.join(app.config['UPLOAD_FOLDER'], user_id)
     file_path = os.path.join(user_folder, file_name)
 
     try:
         if os.path.exists(file_path) and file_name.endswith(".pdf"):
-            # Determine the content type based on the file extension
             content_type = "application/pdf"
-
-            # Create a response with the file data and set headers
             response = make_response(send_file(file_path, mimetype=content_type, as_attachment=True, download_name=file_name))
             response.headers["Content-Type"] = content_type
 
@@ -103,5 +172,14 @@ def open_file(user_id, file_name):
     except Exception as e:
         return jsonify({'error': str(e)}), 500
     
+@app.route('/test', methods=['GET'])
+@check_jwt
+def testing():
+    try:
+        return jsonify({'message': 'Hello World!'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port ,debug=True)
